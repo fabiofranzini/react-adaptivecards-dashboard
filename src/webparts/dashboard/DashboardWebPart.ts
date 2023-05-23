fe76ba5811fb552fb4d396d77f85bebad188b9ef
Fabio Franzini <fabio@fabiofranzini.com>
2023-05-23 14:34:01 +0200

    fix

--- src/webparts/dashboard/DashboardWebPart.ts
+++ src/webparts/dashboard/DashboardWebPart.ts
@@ -58,7 +58,7 @@ export default class DashboardWebPart extends BaseClientSideWebPart<IDashboardWe
       pages: [
         {
           header: {
-            description: strings.PropertyPaneDescription
+            description: null
           },
           groups: [
             {
